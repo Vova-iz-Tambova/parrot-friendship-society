a92d1dcdfd3785c45cac23e93c388f4ac66de621
Vova-iz-Tambova <voller.ruden@yandex.ru>
2023-07-25 14:46:49 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+// файл App.js
+
+import React from 'react';
 import './App.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import serverData from '../serverData';
+// импортируем нужные компоненты
+import Friends from './Friends';
+import Friend from './Friend';
+import Dashboard from './Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <header className="header">
+          <NavLink to='/' className="header__logo">Parrot Friendship Society</NavLink>
+          <nav className="menu">
+            <ul className="menu__list">
+              <li className="menu__list-item"><NavLink className="menu__link" to='/friends'>Друзья</NavLink></li>
+              <li className="menu__list-item"><NavLink className="menu__link" to='/news'>Новости</NavLink></li>
+            </ul>
+          </nav>
+        </header>
+        <Routes>
+          <Route exact path='/' element={<Dashboard />} />
+          <Route exact path='/friends' element={<Friends serverData={serverData} />} />
+          <Route path='/friends/:id' element={<Friend serverData={serverData} />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
